05d160e2ef82493c856e33fbaa17ccc6a2fb2dfa
Karim Mostafa <karim.mostafa.18@gmail.com>
2017-02-03 05:03:59 +0200

    Add more info and interaction to recyclerview item

--- app/src/main/java/com/realkarim/apps/imagestore/SearchImageRecyclerViewAdapter.java
+++ app/src/main/java/com/realkarim/apps/imagestore/SearchImageRecyclerViewAdapter.java
@@ -2,10 +2,16 @@ package com.realkarim.apps.imagestore;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.realkarim.apps.imagestore.getty.models.Image;
 import com.squareup.picasso.Picasso;
@@ -23,21 +29,35 @@ public abstract class SearchImageRecyclerViewAdapter extends RecyclerView.Adapte
 
     private Integer page;
     private Integer pageSize;
+    private LayoutInflater layoutInflater;
+    private String TAG = getClass().getName();
+
+    PopupWindow pw;
 
     public SearchImageRecyclerViewAdapter(Context context) {
         imageLists = new ArrayList<>();
         this.context = context;
-
+        layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         page = 1;
         pageSize = 10;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
+        final View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_search_recyclerview_item, parent, false);
 
-        ViewHolder viewHolder = new ViewHolder(view);
+        final ViewHolder viewHolder = new ViewHolder(view);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer position = viewHolder.getAdapterPosition();
+                if (viewHolder.getAdapterPosition() != RecyclerView.NO_POSITION)
+                    showPopupWindow(v, position);
+            }
+        });
         return viewHolder;
     }
 
@@ -48,6 +68,9 @@ public abstract class SearchImageRecyclerViewAdapter extends RecyclerView.Adapte
                 .error(R.drawable.photo_not_available)
                 .placeholder(R.drawable.loading)
                 .into(holder.image);
+
+        holder.imageTitle.setText(imageLists.get(position).getTitle());
+        holder.imageID.setText("ID: " + imageLists.get(position).getId());
 
         // load more when reaching the end of the list
         if (position == imageLists.size() - 1) {
@@ -62,12 +85,16 @@ public abstract class SearchImageRecyclerViewAdapter extends RecyclerView.Adapte
         return imageLists.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         public ImageView image;
+        public TextView imageTitle;
+        public TextView imageID;
 
         public ViewHolder(View v) {
             super(v);
             image = (ImageView) v.findViewById(R.id.image);
+            imageTitle = (TextView) v.findViewById(R.id.image_title);
+            imageID = (TextView) v.findViewById(R.id.image_id);
         }
     }
 
@@ -76,12 +103,46 @@ public abstract class SearchImageRecyclerViewAdapter extends RecyclerView.Adapte
         this.notifyDataSetChanged();
     }
 
-    public void clearList(){
+    public void clearList() {
         imageLists.clear();
         page = 1;
         pageSize = 10;
         // notifyDataSetChanged() will be called when the next page is added
     }
 
+    private void showPopupWindow(View v, Integer position) {
+        try {
+            //Inflate the view from a predefined XML layout
+            View layout = layoutInflater.inflate(R.layout.recyclerview_item_popup,
+                    ((ViewGroup) v.getParent()), false);
+
+            // create PopupWindow
+            final PopupWindow pw = new PopupWindow(
+                    layout,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT, true);
+            // display the popup in the center
+            pw.showAtLocation(v.getRootView(), Gravity.CENTER, 0, 0);
+
+            TextView title = (TextView) layout.findViewById(R.id.image_title);
+            TextView caption = (TextView) layout.findViewById(R.id.image_caption);
+            Button dismiss = (Button) layout.findViewById(R.id.dismiss);
+
+            title.setText("Title: " + imageLists.get(position).getTitle());
+            caption.setText(imageLists.get(position).getCaption());
+            dismiss.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pw.dismiss();
+                }
+            });
+
+        } catch (Exception e) {
+            Toast.makeText(context, "Error showing Caption!", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+    }
+
     public abstract void loadMore(Integer nextPage, Integer nextPageSize);
+
 }
